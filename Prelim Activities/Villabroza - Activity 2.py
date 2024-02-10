565041c32f2affc7b7527c96adc01bb293d2cf48
DarkLightBeat <153878111+DarkLightBeat@users.noreply.github.com>
2024-02-10 20:54:15 +0800

    Update Villabroza - Activity 2.py

--- Prelim Activities/Villabroza - Activity 2.py
+++ Prelim Activities/Villabroza - Activity 2.py
@@ -1,42 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
+import re  # Importing the regular expression module
 
-# In[68]:
+x = open("regex_sum_42.txt")  # Opening the file named "regex_sum_42.txt"
+c = 0  # Initializing a variable to store the sum of all numbers found in the file
+g = 0  # Initializing a variable to store the total vowel count in the file
+j = 0  # Initializing a variable to store the count of occurrences of the word "Python" in the file
+num_list = []  # Initializing an empty list to store all numbers found in the file
 
-
-import re
-
-x = open("regex_sum_42.txt")
-c = 0
-g = 0
-j = 0
-num_list = []
-
+# Looping through each line in the file
 for i in x:
-    i = i.rstrip()
-    if re.search("^Python+", i):
-        print(i)
+    i = i.rstrip()  # Removing any trailing whitespaces from the current line
+    
+    if re.search("^Python+", i):  # Searching for lines that start with "Python"
+        print(i)  # Printing those lines
         
-
-    a = re.findall(r"\d+", i)
+    a = re.findall(r"\d+", i)  # Finding all numeric substrings in the current line
     for b in a:
-        c = c + int(b)
-        num_list.append(int(b))
+        c = c + int(b)  # Adding each numeric substring to the sum
+        num_list.append(int(b))  # Appending each numeric substring to the list of numbers
         
-
-    d = re.findall(r"[aeiouAEIOU]+", i)
+    d = re.findall(r"[aeiouAEIOU]+", i)  # Finding all sequences of vowels in the current line
     for e in d:
         for f in e:
-            g = g + 1
+            g = g + 1  # Counting each vowel occurrence
             
-   
-    h = re.findall("Python", i)
+    h = re.findall("Python", i)  # Finding all occurrences of the word "Python" in the current line
     for i in h:
-        j = j + 1
+        j = j + 1  # Counting each occurrence of the word "Python"
 
+# Printing the results
 print()
 print("The sum of all numbers is", c)
 print("The highest number is",max(num_list))
 print("The total vowel count is", g)
 print("Python word count is", j)
-
